1bb103bbaa81986d79ff6f8775af703dfeee38f2
unknown <ivankaboiko06@gmail.com>
2019-05-30 19:25:06 +0300

    push all files

---
+++ src/test/java/TestGmail.java
@@ -0,0 +1,53 @@
+import bo.SignInBO;
+import bo.WriteMessageBO;
+import driver.DriverManager;
+import listener.MyCustomListener;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import utilits.json.Parser;
+import utilits.model.UserModel;
+import utilits.properties.FilePath;
+
+import java.io.File;
+
+@Listeners({MyCustomListener.class})
+class TestGmail {
+    private Logger logger = LogManager.getLogger(TestGmail.class);
+
+    @DataProvider(parallel = true)
+    public Object[] getUsers() {
+        FilePath filePath = new FilePath();
+        File json = new File(filePath.propertyFile("fileWayJSON"));
+        Parser parser = new Parser();
+        return parser.getData(json);
+    }
+
+    @Test(dataProvider = "getUsers")
+    public void LoginTest(UserModel user) {
+        SignInBO signIn = new SignInBO();
+        signIn.login(user.getLogin(), user.getPassword());
+        logger.info("You have successfully logged in.");
+
+        WriteMessageBO writeMessage = new WriteMessageBO();
+        writeMessage.tryToWriteMessage(
+                user.getMessageModel().getReceiver(),
+                user.getMessageModel().getSubject(),
+                user.getMessageModel().getMessage());
+        logger.info("Message was wrote but was not sent.");
+
+        Assert.assertTrue(writeMessage.isSaveInDraft(user.getMessageModel().getSubject()));
+        logger.info("Message was saved as a draft.");
+        writeMessage.sendMessage();
+        logger.info("Message was sent.");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        DriverManager.tearDown();
+    }
+}
---
+++ src/test/java/listener/MyCustomListener.java
@@ -0,0 +1,39 @@
+package listener;
+
+import driver.DriverManager;
+import io.qameta.allure.Attachment;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestResult;
+import org.testng.TestListenerAdapter;
+
+import java.util.Objects;
+
+public class MyCustomListener extends TestListenerAdapter {
+    private Logger logger = LogManager.getLogger(MyCustomListener.class);
+
+    public void onTestStart(ITestResult result){
+        logger.info("Test class started: " + result.getTestClass().getName());
+        logger.info("Test started: " + result.getName());
+    }
+
+    public void onTestSuccess(ITestResult result){
+        logger.info("Test success: " + result.getName());
+    }
+
+    public void onTestFailure(ITestResult result){
+        getScreenshot();
+        logger.info("Test failed: " + result.getName());
+        if(Objects.nonNull(result.getThrowable())){
+            result.getThrowable().printStackTrace();
+        }
+    }
+
+    @Attachment(value = "Page screenshot", type = "image/png")
+    private byte[] getScreenshot(){
+        return ((TakesScreenshot) DriverManager.getDriver()).getScreenshotAs(OutputType.BYTES);
+    }
+
+}
