eff057222c090e1f533affb7ed82161e6990e6b6
Romana Chykalo <romo4kachukalo@gmail.com>
2019-06-02 15:19:48 +0300

    reportNG

--- src/main/java/listener/CustomListener.java
+++ src/main/java/listener/CustomListener.java
@@ -10,16 +10,14 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.Reporter;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.Listeners;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.GregorianCalendar;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 import static java.lang.String.format;
 
@@ -27,63 +25,67 @@ import static java.lang.String.format;
 public class CustomListener implements ITestListener {
 
     private static Logger log = LogManager.getLogger(CustomListener.class);
+    private String logPath = "Logs/test-yaml.log";
 
     @Override
     public void onStart(ITestContext context) {
-        log.info("Test with name: " + context.getName() + ", STARTED in time: " + context.getStartDate().toString());
+        log.info("------------------TEST START---------------------- ");
+        log.info("TEST STARTED in time: " + context.getStartDate().toString());
     }
 
     @Override
     public void onFinish(ITestContext context) {
-        log.info("Test completed on: " + context.getEndDate().toString());
+        log.info("TEST FINISHED in time: " + context.getEndDate().toString());
+        log.info("------------------TEST FINISH---------------------- ");
+        removeLogs();
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
-        log.info(format("result : SUCCESS : %s", result.getMethod().getMethodName()));
+        log.info(format(" Result : SUCCESS : %s", result.getMethod().getMethodName()));
+        appendLogToReport();
+        Reporter.setCurrentTestResult(null);
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
-        log.info("result : FAILURE " + result.getMethod().getMethodName().toUpperCase());
-        System.setProperty("org.uncommons.reportng.escape-output", "false");
+        log.error("result : FAILURE " + result.getMethod().getMethodName().toUpperCase());
+        saveScreenshotToReport(result);
+        appendLogToReport();
+        Reporter.setCurrentTestResult(null);
+    }
+
+    private void appendLogToReport() {
+        try {
+            System.setProperty("org.uncommons.reportng.escape-output", "false");
+            Path path = Paths.get(logPath);
+            List<String> strings = Files.readAllLines(path);
+            strings.stream().distinct().forEach(s -> Reporter.log(s + "</br>"));
+        } catch (IOException e) {
+            log.error("Can't read logs to Report");
+        }
+    }
+
+    private void saveScreenshotToReport(ITestResult result) {
         if (!result.isSuccess()) {
             try {
+                System.setProperty("org.uncommons.reportng.escape-output", "false");
                 String failureImageFileName = new SimpleDateFormat("MM-dd-yyyy_HH-ss").format(new GregorianCalendar().getTime());
-                String failureImageFileName1;
                 File scrFile = ((TakesScreenshot) DriverLoader.getDriver()).getScreenshotAs(OutputType.FILE);
                 FileUtils.copyFile(scrFile, new File(failureImageFileName));
                 String userDirector = System.getProperty("user.dir") + "/";
-                String s1 = "<table><tr><td><font style=\"text-decoration: underline;\" " +
+                String screenCapture = "<table><tr><td><font style=\"text-decoration: underline;\" " +
                         "size=\"3\" face=\"Comic sans MS\" color=\"green\"><b>Screenshot</b></font></td></tr> ";
-                Reporter.log(s1);
-                Reporter.log("<tr><td><a href=\"" + userDirector + failureImageFileName + "\"><img src=\"file:///" + userDirector + failureImageFileName + "\" alt=\"\"" + "height=’120′ width=’120′/></td></tr> ");
-                appendLogToReport();
-                Reporter.setCurrentTestResult(null);
+                Reporter.log(screenCapture);
+                Reporter.log("<tr><td><a href=\"" + userDirector + failureImageFileName + "\"><img src=\"file:///"
+                        + userDirector + failureImageFileName + "\" alt=\"\"" + "height=’120′ width=’120′/></td></tr> ");
             } catch (IOException e1) {
                 e1.printStackTrace();
             }
         }
     }
-    private void appendLogToReport() {
-        try {
-            System.setProperty("org.uncommons.reportng.escape-output", "false");
-            log.info("Start read logs...........");
-            Scanner s = new Scanner(new File("Logs/test-yaml.log"));
-            List<String> list = new ArrayList<>();
-            while (s.hasNext()) {
-                list.add(s.next());
-            }
-            for (String l : list) {
-                Reporter.log(l);
-            }
-            s.close();
-        } catch (IOException e) {
-            log.error("Can't read logs to Allure Report");
-        }
-    }
 
-        @Override
+    @Override
     public void onTestSkipped(ITestResult result) {
         log.info(result.getMethod().getMethodName() + "result : SKIPPED");
     }
@@ -91,6 +93,14 @@ public class CustomListener implements ITestListener {
     @Override
     public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
         log.info(("onTestFailedButWithinSuccessPercentage for " + result.getMethod().getMethodName()));
+    }
+
+    public void removeLogs() {
+        try {
+            FileUtils.write(new File(logPath), "", "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
