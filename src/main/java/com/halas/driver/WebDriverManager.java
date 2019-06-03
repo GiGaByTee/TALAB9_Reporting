97e34e641e84a7080e6ddf8baa3752160ec4c25a
Yurii Halas <galasayra123213@gmail.com>
2019-06-03 12:17:48 +0300

    init

---
+++ src/main/java/com/halas/driver/WebDriverManager.java
@@ -0,0 +1,45 @@
+package com.halas.driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+import static com.halas.util.property.HandleProperty.getValueProperty;
+
+public final class WebDriverManager {
+    private static final ThreadLocal<WebDriver> DRIVER_POOL = new ThreadLocal<>();
+
+    static {
+        System.setProperty(getValueProperty("name"), getValueProperty("path"));
+    }
+
+    private WebDriverManager() {
+    }
+
+    public static WebDriver getWebDriver() {
+        if (Objects.isNull(DRIVER_POOL.get())) {
+            createWebDriver();
+        }
+        return DRIVER_POOL.get();
+    }
+
+    public static void shutdown() {
+        DRIVER_POOL.get().quit();
+        DRIVER_POOL.set(null);
+    }
+
+    private static void createWebDriver() {
+        DRIVER_POOL.set(getChromeDriver());
+    }
+
+    private static ChromeDriver getChromeDriver(){
+        ChromeDriver chromeDriver = new ChromeDriver();
+        chromeDriver.manage().window().maximize();
+        chromeDriver.manage()
+                .timeouts()
+                .implicitlyWait(Integer.valueOf(getValueProperty("implicit-wait-time")), TimeUnit.SECONDS);
+        return chromeDriver;
+    }
+}
